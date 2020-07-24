573690dca385a9e72f28b5f9ee266f69e293a4e4
shahrukh <shahrukh@gmail.com>
2020-07-24 18:44:22 +0530

    modified dashboard

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import loginView
+from .views import loginView,logOut,dashBoard
 
 urlpatterns = [
     path('login/',loginView.as_view(),name='login'),
+    path('logout/',logOut,name='logout'),
+    path('dashboard/',dashBoard,name='dashboard'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import View
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from products.models import Products
+from django.contrib.auth.decorators import login_required
 
 class loginView(View):
 
@@ -9,15 +11,23 @@ class loginView(View):
         return render(self.request,'accounts/Login.html')
 
     def post(self,request):
+        object_count = Products.objects.all().count()
         email = request.POST.get('email')
         password = request.POST.get('password')
         user = authenticate(self.request,email=email,password=password)
         if user is not None:
             messages.success(self.request,"User Loggined successfully")
             login(self.request,user)
-            return render(self.request, 'dashboard.html')
+            return render(self.request, 'dashboard.html',{'product_count':object_count})
         else:
             messages.error(self.request,"Invalid username or Passowrd")
             return render(self.request, 'accounts/Login.html')
 
+def logOut(request):
+    logout(request)
+    return redirect('/login')
 
+@login_required
+def dashBoard(request):
+    object_count = Products.objects.all().count()
+    return render(request, 'dashboard.html',{'product_count':object_count})
