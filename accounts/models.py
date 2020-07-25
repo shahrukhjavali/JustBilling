3b7382ccbe60643aca29f60ab4e0d129ddeb5a00
shahrukh <shahrukh@gmail.com>
2020-07-25 11:28:58 +0530

    Added User Management

--- accounts/models.py
+++ accounts/models.py
@@ -2,14 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class UserManager(BaseUserManager):
-    def create_user(self,email,password=None):
+    def create_user(self,email,password,*args,**kwargs):
         if not email:
             raise ValueError("Users must have an email address")
-
         user = self.model(
-            email=self.normalize_email(email),
+            email=UserManager.normalize_email(email)
         )
         user.set_password(password)
+        user.usr_role = 'Excutive'
+        user.active = True
+        user.staff = True
         user.save(using=self._db)
         return user
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,33 +1,60 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from products.models import Products
+from accounts.models import User
+from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+
 
 class loginView(View):
 
-    def get(self,request):
-        return render(self.request,'accounts/Login.html')
+    def get(self, request):
+        return render(self.request, 'accounts/Login.html')
 
-    def post(self,request):
+    def post(self, request):
         object_count = Products.objects.all().count()
         email = request.POST.get('email')
         password = request.POST.get('password')
-        user = authenticate(self.request,email=email,password=password)
+        user = authenticate(self.request, email=email, password=password)
         if user is not None:
-            messages.success(self.request,"User Loggined successfully")
-            login(self.request,user)
-            return render(self.request, 'dashboard.html',{'product_count':object_count})
+            messages.success(self.request, "User Loggined successfully")
+            login(self.request, user)
+            return render(self.request, 'dashboard.html', {'product_count': object_count})
         else:
-            messages.error(self.request,"Invalid username or Passowrd")
+            messages.error(self.request, "Invalid username or Passowrd")
             return render(self.request, 'accounts/Login.html')
+
 
 def logOut(request):
     logout(request)
     return redirect('/login')
 
+class Userprofile(View):
+    def get(self, request):
+        allusers = User.objects.all().order_by('-id')
+        paginator = Paginator(allusers, 10)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(self.request,'user/users.html', {'user_all': page_obj})
+
+    def post(self,request):
+        email = str(request.POST.get('email'))
+        #confirmpass = request.POST.get('confirmpassword')
+        password = request.POST.get('password')
+        print(password)
+        user = User.objects.create_superuser(self,password,email)
+        allusers = User.objects.all().order_by('-id')
+        paginator = Paginator(allusers, 10)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(self.request, 'user/users.html', {'user_all': page_obj})
+
 @login_required
 def dashBoard(request):
     object_count = Products.objects.all().count()
-    return render(request, 'dashboard.html',{'product_count':object_count})
+    return render(request, 'dashboard.html', {'product_count': object_count})
+
+
+
